e0c20bdf839a1086110534262b4e0732d888434b
Liudas Sodonis aka lfx aka lso <lfx@users.noreply.github.com>
2018-12-17 13:59:17 -0800

    added front end

--- src/index.js
+++ src/index.js
@@ -3,19 +3,44 @@ import ReactDOM from "react-dom";
 import Upload from "./Upload.jsx";
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <div>Chekc Console</div>
-      <Upload
-        onDone={fileData => {
-          console.log("filedata", JSON.stringify(fileData));
-        }}
-      />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      files: []
+    };
+  }
+
+  getFiles(files) {
+    this.setState({ files: files });
+  }
+
+  render() {
+    console.log(this.state.files, this.state.files[0]);
+    debugger;
+    return (
+      <div>
+        <div className="text-center mt-25">
+          <p className="text-center">Upload img</p>
+          <Upload onDone={fileData => this.getFiles(fileData)} />
+        </div>
+        <div className="text-center">
+          {this.state.files.length !== 0 ? (
+            <img alt="img" src={this.state.files.base64} />
+          ) : null}
+        </div>
+
+        {this.state.files.length !== 0 ? (
+          <div>
+            <h3 className="text-center mt-25">Object</h3>
+            <div className="pre-container">
+              <pre>{JSON.stringify(this.state.files, null, 2)}</pre>
+            </div>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
